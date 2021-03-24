c0b060aa122e6ba73e8b87ab7743c73a032ca60c
stark136 <81159077+stark136@users.noreply.github.com>
2021-03-24 20:43:29 +0530

    Add files via upload

---
+++ ascll3.py
@@ -0,0 +1,49 @@
+import os,time,colorama
+from colorama import Fore,Back, Style
+colorama.init()
+print(Fore.BLUE+'''
+
+███╗   ███╗██╗   ██╗███████╗██╗ ██████╗    ██████╗ ██╗      █████╗ ██╗   ██╗███████╗██████╗ 
+████╗ ████║██║   ██║██╔════╝██║██╔════╝    ██╔══██╗██║     ██╔══██╗╚██╗ ██╔╝██╔════╝██╔══██╗
+██╔████╔██║██║   ██║███████╗██║██║         ██████╔╝██║     ███████║ ╚████╔╝ █████╗  ██████╔╝
+██║╚██╔╝██║██║   ██║╚════██║██║██║         ██╔═══╝ ██║     ██╔══██║  ╚██╔╝  ██╔══╝  ██╔══██╗
+██║ ╚═╝ ██║╚██████╔╝███████║██║╚██████╗    ██║     ███████╗██║  ██║   ██║   ███████╗██║  ██║
+╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚═╝ ╚═════╝    ╚═╝     ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝''')
+                                                                                            
+print(Fore.RESET)
+print(Fore.YELLOW+          "MADE BY ANSHUMAAN SHARMA @STARK IN INDIA")
+print(Fore.RESET)
+print(Fore.GREEN)
+z=input("ENTER THE PASSWORD TO PROCCED:-")
+print(Fore.RESET)
+print(Fore.YELLOW)
+if 'stark' in z:
+
+  
+
+
+
+  filenames=("music.py","music2.py")
+  frames=[]
+  os.system('cls')
+
+  for name in filenames:
+    with open(name,"r",encoding="utf8") as f:
+        frames.append(f.readlines())
+
+
+  a=10
+  while a>=10:
+
+   
+   
+   for frame in frames:
+     
+    
+     print("".join(frame))
+     
+     time.sleep(0.02)
+     
+     os.system('cls')
+
+    
